7b88ca12f1bb29a8baaa6bc133de453f8bc35975
Yawaad <143392659+Yawaad@users.noreply.github.com>
2023-09-07 13:49:08 +0300

    weapon rotation comm
    i added comments on each line so you can understand how the code works

--- ZombShshs/Assets/PlayerInput.cs
+++ ZombShshs/Assets/PlayerInput.cs
@@ -5,27 +5,43 @@ using UnityEngine.InputSystem;
 
 public class PlayerInput : MonoBehaviour
 {
+    // Reference to an InputAction asset that provides pointer position information.
     [SerializeField] InputActionReference pointerPosition;
 
+    // Store the current pointer input in a private variable.
     private Vector2 pointerInput;
+
+    // A property to access the pointer input externally.
     private Vector2 PointerInput => pointerInput;
+
+    // Reference to the WeaponParent component attached to the player.
     private WeaponParent weaponParent;
 
     private void Awake()
     {
+        // Find and store the WeaponParent component in the player's children.
         weaponParent = GetComponentInChildren<WeaponParent>();
     }
 
     private void Update()
     {
+        // Get the current pointer input and assign it to pointerInput.
         pointerInput = GetPointerInput();
+
+        // Update the PointerPosition property of the weapon parent with the pointer input.
         weaponParent.PointerPosition = pointerInput;
     }
 
+    // Function to retrieve the pointer input.
     private Vector2 GetPointerInput()
     {
+        // Read the input value from the pointerPosition InputActionReference.
         Vector3 mousePos = pointerPosition.action.ReadValue<Vector2>();
+
+        // Set the z-component of the input to the camera's near clip plane for proper conversion.
         mousePos.z = Camera.main.nearClipPlane;
+
+        // Convert the screen position to world space and return it as the pointer input.
         return Camera.main.ScreenToWorldPoint(mousePos);
     }
 }
