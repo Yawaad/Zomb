9a4f8a22b357959236cd4219c08e866fb4e50f19
YoussifDarwish <youssifmhmoud7@gmail.com>
2023-09-07 21:40:44 +0200

    Update playermovement.cs

--- ZombShshs/Assets/Script/playermovement.cs
+++ ZombShshs/Assets/Script/playermovement.cs
@@ -8,8 +8,9 @@ public class playermovement : MonoBehaviour
 
     Rigidbody2D rb;
     WeaponParent bulletdirection;
-
+    //to get the bullet prefab for instantiation
     [SerializeField] GameObject BulletPF;
+    //to get the posiiton and rotation of the bullet
     [SerializeField] Transform WeaponEnd;
     [SerializeField] Transform BulletDirection;
     
@@ -23,15 +24,15 @@ public class playermovement : MonoBehaviour
 
     public void Move(InputAction.CallbackContext button)
     {
-        if(button.performed)
+        if(button.performed) //if the button for movement is clicked
         {
-            Vector2 direction = button.ReadValue<Vector2>().normalized;
-            rb.velocity = new Vector2(direction.x, direction.y).normalized * 5;
-            Debug.Log(direction);
+            Vector2 direction = button.ReadValue<Vector2>().normalized; //get the value in vector2
+            rb.velocity = new Vector2(direction.x, direction.y).normalized * 5; //apply movement
+    
         }
         else
         {
-            rb.velocity = Vector2.zero;
+            rb.velocity = Vector2.zero; //if no button is clicked, stop
         }
         
     }
@@ -40,7 +41,7 @@ public class playermovement : MonoBehaviour
     {
         if (button.performed)
         {
-           Instantiate(BulletPF, WeaponEnd.position, BulletDirection.rotation);
+           Instantiate(BulletPF, WeaponEnd.position, BulletDirection.rotation); //create a bullet
            
         }
            
